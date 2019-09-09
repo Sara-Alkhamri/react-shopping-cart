b8b11eb04ede7713091f2d6ffb3b712aa34ac910
Sara Alkhamri <s.alkhamri@gmail.com>
2019-09-09 12:00:49 -0700

    Refactored Products Route to no longer use render props

--- src/App.js
+++ src/App.js
@@ -24,17 +24,9 @@ function App() {
 			<Navigation cart={cart} />
 
 			{/* Routes */}
-			<Route
-				exact
-				path="/"
-				render={() => (
-					<Products
-						products={products}
-						addItem={addItem}
-					/>
+			<Route exact path="/" component={Products} />
 				)}
-			/>
-
+				
 			<Route
 				path="/cart"
 				render={() => <ShoppingCart cart={cart} />}
