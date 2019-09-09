883361552c5dc0ed5b6bc146ae81eba0eb85b9f8
Sara Alkhamri <s.alkhamri@gmail.com>
2019-09-09 12:09:29 -0700

    Called useContext and passed in the context object (prouctProductContext) in Products.js

--- src/components/Products.js
+++ src/components/Products.js
@@ -7,6 +7,7 @@ import Product from './Product';
 import ProductContext from '../contexts/ProductContext';
 
 const Products = props => {
+	const { products, addItem } = useContext(ProductContext);
 	return (
 		<div className="products-container">
 			{props.products.map(product => (
